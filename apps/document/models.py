2d37f8e5a9367d1d8a19178b3320b9c0647b980e
Vitoriano <vitorianoernandes2012@gmail.com>
2020-05-04 23:55:21 -0300

    defining APP relationships

--- apps/document/models.py
+++ apps/document/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.employee.models import Employee
 
 class Document(models.Model):
     description = models.CharField(max_length=100, help_text='Document description')
+    employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.description
--- apps/overtime_record/models.py
+++ apps/overtime_record/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.employee.models import Employee
 
 class Overtime_record(models.Model):
     reason = models.CharField(max_length=100, help_text='Overtime reason')
+    employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.reason
