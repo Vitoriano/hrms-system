bc54a68f2a766d3601b444fe92b60e5ae094d0f6
Vitoriano <vitorianoernandes2012@gmail.com>
2020-05-04 23:32:25 -0300

    Overtime APP

--- apps/overtime_record/admin.py
+++ apps/overtime_record/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Overtime_record
 
-# Register your models here.
+admin.site.register(Overtime_record)
--- apps/overtime_record/models.py
+++ apps/overtime_record/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Overtime_record(models.Model):
+    reason = models.CharField(max_length=100, help_text='Overtime reason')
+
+    def __str__(self):
+        return self.reason
