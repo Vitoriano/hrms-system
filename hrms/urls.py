69a9eeef93f4a63f875808860d3cd537b07b545b
Vitoriano <vitorianoernandes2012@gmail.com>
2020-05-13 00:02:11 -0300

    company generic view create

--- hrms/urls.py
+++ hrms/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('employee/', include('apps.employee.urls')),
+    path('company/', include('apps.company.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
