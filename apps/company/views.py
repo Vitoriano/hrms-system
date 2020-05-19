356607ab2a8c92c9a73a2d75c70e902faf967eb3
Vitoriano <vitorianoernandes2012@gmail.com>
2020-05-18 22:23:13 -0300

    bind employee company

--- apps/company/views.py
+++ apps/company/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Company
@@ -5,3 +6,10 @@ from .models import Company
 class CompanyCreate(CreateView):
     model = Company
     fields = ['name']
+
+    def form_valid(self, form):
+        obj = form.save()
+        employee = self.request.user.employee
+        employee.company = obj
+        employee.save()
+        return HttpResponse('ok')
